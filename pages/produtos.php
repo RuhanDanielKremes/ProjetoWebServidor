57f35c785104af92c98df47da56213c7b511d126
Ruhan Daniel Kremes <ruhankremes@alunos.utfpr.edu.br>
2024-04-07 15:58:18 -0300

    diretory reorganization
    diretory reorganization and added product

--- pages/produtos.php
+++ pages/produtos.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-<!DOCTYPE html>
-<html lang="en">
-
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
 <?php include 'templates/header.php'?>
-<div><img src="Patins.png" alt=""></div>
+<div class='container mediun'>
+    <img src="Patins.png" alt="">
+    <br>
+    <span>Valor R$5.000.000.000.000,00</span>
+    <button class='btn buy'>Comprar</button>
+</div>
 <?php include 'templates/footer.php'?>
+</body>
 </html>
