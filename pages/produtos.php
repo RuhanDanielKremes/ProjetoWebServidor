643211045f83f8b7d06b7025a07f8e4545f44f3b
JuliarrCosta <Juliaraquelrc@outlook.com>
2024-04-10 23:12:14 -0300

    Pequena alteração

--- pages/produtos.php
+++ pages/produtos.php
@@ -3,12 +3,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
 <?php include 'templates/header.php'?>
-<?php include 'templates/DB.php'?>
+
 <div class='container mediun'>
     <h1>PÁGINA DE PRODUTOS</h1>
     <!--GRID DE PRODUTOS-->
     <div class="grid-product">
         <?php 
+            require 'DB.php';
             //A cada iteração vai renderizar um card de produtos vindo do banco de dados
             foreach($DB_PRODUTOS['image_product'] as $index => $image){
                 $name = $DB_PRODUTOS['name_product'][$index];
