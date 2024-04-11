b1815a83fdb5845b4280fe7ae9722b537c4b1a95
JuliarrCosta <Juliaraquelrc@outlook.com>
2024-04-10 23:09:24 -0300

    BD produtos e página

--- pages/produtos.php
+++ pages/produtos.php
@@ -3,11 +3,27 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
 <?php include 'templates/header.php'?>
+<?php include 'templates/DB.php'?>
 <div class='container mediun'>
-    <img src="Patins.png" alt="">
-    <br>
-    <span>Valor R$5.000.000.000.000,00</span>
-    <button class='btn buy'>Comprar</button>
+    <h1>PÁGINA DE PRODUTOS</h1>
+    <!--GRID DE PRODUTOS-->
+    <div class="grid-product">
+        <?php 
+            //A cada iteração vai renderizar um card de produtos vindo do banco de dados
+            foreach($DB_PRODUTOS['image_product'] as $index => $image){
+                $name = $DB_PRODUTOS['name_product'][$index];
+                $price = $DB_PRODUTOS['price_product'][$index];
+                $quantity = $DB_PRODUTOS['quantity_product'][$index];
+        ?>
+            <div class="card-product">
+            <img src="<?php echo $image; ?>" alt="<?php echo $name; ?>">
+                <h2><?php echo $name; ?></h2>
+                <p>Preço: R$ <?php echo number_format($price, 2, ',', '.'); ?></p>
+                <p>Quantidade disponível: <?php echo $quantity; ?></p>
+                <button class="btn-buy">Comprar</button>
+            </div>
+            <?php } ?>
+    </div>
 </div>
 <?php include 'templates/footer.php'?>
 </body>
--- pages/templates/DB.php
+++ pages/templates/DB.php
@@ -1,5 +1,5 @@
 <?php
-
+    /*BANCO DE DADOS CADASTRO*/ 
     $DB = [
         'name' => ['Ruhan', 'user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'kaue', 'Julia'],
         'email' => ['admin@gmail.com', 'user1@gmail.com', 'user2@gmail.com', 'user3@gmail.com', 'user4@gmail.com', 'user5@gmail.com', 'user6@gmail.com', 'user7@gmail.com', 'user8@gmail.com', 'user9@gmail.com'],
@@ -7,6 +7,20 @@
         'phone' => ['+1 00 0000 0000', '+1 11 1111 1111', '+1 22 2222 2222', '+1 33 3333 3333', '+1 44 4444 4444', '+1 55 5555 5555', '+1 66 6666 6666', '+1 77 7777 7777', '+1 88 8888 8888', '+1 99 9999 9999'],
         'role' => ['admin', 'user', 'user', 'user', 'user', 'user', 'user', 'user', 'user', 'admin', 'admin'],
         'created_at' => ['created_at', 'created_at', 'created_at', 'created_at', 'created_at', 'created_at', 'created_at', 'created_at', 'created_at', 'created_at']
+    ];
+
+    /* Adicionei um banco de dados para os produtos para poder 
+        manipular no carrinho facilmente, visto quese fizer estático com html e css a manipulação ficaria complexa
+        e poderiamos nos deparar com um problema de lógica lá na frente 
+        Optei apenas por fazer a descrição estática -JC
+        */
+
+    /*BANCO DE DADOS DOS PRODUTOS*/
+    $DB_PRODUTOS= [
+        'image_product' => ['../images/product-1.png','../images/product-2.png','../images/product-3.png','../images/product-4.png', '../images/product-5.png', '../images/product-6.png','../images/product-7.png', '../images/product-8.png', '../images/product-9.png'],
+        'name_product' => ['Notebook Dell Aspire', 'Notebook Dell Inspiron 15', 'Notebook Dell G15', 'Kit Gamer', 'Placa de video GTX 1650', 'Controle PS5', 'Cadeira Gamer', 'Iphone 13', 'Galaxy S23 Ultra'],
+        'price_product' => [3950.00, 2500, 5000, 250, 1200, 500, 650, 3900, 3800],
+        'quantity_product' => [5, 3, 3, 10, 5, 6, 3, 5, 7]
     ];
     
 ?>
