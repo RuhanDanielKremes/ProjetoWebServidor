50d72351e7469362224f48471b4caf72d5fd6751
Ruhan Daniel Kremes <ruhankremes@alunos.utfpr.edu.br>
2024-04-11 22:47:48 -0300

    Updated the nav bar
    fixed the bug that just appear in home page("index.php")

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
     session_start();
     if(empty($_SESSION['LogginTime']) || $_SESSION['LogginTime'] < time()){
         header("Location: templates/Login.php");
@@ -23,9 +23,30 @@
     <nav class="white z-depth-0 deep-orange lighten-4">
         <div class="nav-wrapper">
             <ul class="left">
-                <li class="active"><a href="../pages/index.php">Home</a></li>
-                <li class=><a href="../pages/cart_view.php">Carrinho</a></li>
-                <li class=><a href="../pages/produtos-user-view.php">Produtos</a></li>
+                <?php
+                    $atualpage = $_SERVER['REQUEST_URI'];
+                    switch($atualpage){
+                    case '/PWS/ProjetoWebServidor/pages/index.php':
+                        echo '<li class="active"><a href="../pages/index.php">Home</a></li>
+                            <li><a href="../pages/produtos-user-view.php">Produtos</a></li>
+                            <li><a href="../pages/cart_view.php">Carrinho</a></li>';
+                        break;
+                    case '/PWS/ProjetoWebServidor/pages/produtos-user-view.php':
+                        echo '<li><a href="../pages/index.php">Home</a></li>
+                        <li class="active"><a href="../pages/produtos-user-view.php">Produtos</a></li>
+                        <li><a href="../pages/cart_view.php">Carrinho</a></li>';
+                        break;
+                    case 'PWS/ProjetoWebServidor/pages/cart_view.php':
+                        echo '<li><a href="../pages/index.php">Home</a></li>
+                            <li class="active"><a href="../pages/produtos-user-view.php">Produtos</a></li>
+                            <li><a href="../pages/produtos-user-view.php">Produtos</a></li>';
+                        break;
+                    default :
+                        echo '<li><a href="../pages/index.php">Home</a></li>
+                            <li><a href="../pages/produtos-user-view.php">Produtos</a></li>
+                            <li class="active"><a href="../pages/cart_view.php">Carrinho</a></li>';
+                    }
+                ?>
             </ul>
             <ul id="nav-mobile" class="right hide-on-small-and-down ">
                 <li class="paddingLR-10px">
