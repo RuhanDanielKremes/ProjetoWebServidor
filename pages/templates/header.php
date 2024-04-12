d6b7b2f11e120ea498a08d23cb06b806310d6547
Ruhan Daniel Kremes <ruhankremes@alunos.utfpr.edu.br>
2024-04-11 21:55:56 -0300

    Update header.php
    added some buttons for navegation

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -11,27 +11,38 @@
     <title>Hello World</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+        .paddingLR-10px{
+            padding: 0px;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+    </style>
 </head>
 <body class="grey lighten-4">
-    <nav class="white z-depth-0">
-        <div class="container">
-            <a href="#" class="brand-logo brand-text">UwU</a>
-            <ul id="nav-mobile" class="right hide-on-small-and-down">
-                <li>
-                    <div class="black-text border">
+    <nav class="white z-depth-0 deep-orange lighten-4">
+        <div class="nav-wrapper">
+            <ul class="left">
+                <li class="active"><a href="../pages/index.php">Home</a></li>
+                <li class=><a href="../pages/cart_view.php">Carrinho</a></li>
+                <li class=><a href="../pages/produtos-user-view.php">Produtos</a></li>
+            </ul>
+            <ul id="nav-mobile" class="right hide-on-small-and-down ">
+                <li class="paddingLR-10px">
+                    <div class="black-text margin-10px">
                         <?php
                         $timeremain = $_SESSION['LogginTime'] - time();
                         echo 'Tempo restante: ' . $timeremain . ' segundos';
                         ?>
-                    <div>
+                    </div>
                 </li>
-                <li><span>....</span></li>
                 <li>
                     <form method="POST" action="templates/LogOut.php">
                         <button class="btn brand z-depth-10">Log out</button>
                     </form>
                 </li>
             </ul>
+            <a href="#" class="brand-logo center brand-text hide-on-small-and-down">UwU</a>
         </div>
     </nav>
    
