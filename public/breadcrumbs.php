be8d5c14634ba6da3bdd058116e6477cc5c10834
Ruhan Daniel Kremes <ruhankremes@alunos.utfpr.edu.br>
2024-04-13 18:59:50 -0300

    not relevant push

--- public/breadcrumbs.php
+++ public/breadcrumbs.php
@@ -10,26 +10,4 @@ $directories = explode("/", $relativePath);
 // Criar o caminho relativo até o diretório raiz (voltar para o diretório raiz)
 $breadcrumbs = str_repeat("../", count($directories) - 1);
 
-// Adicionar o nome do arquivo índice (index.php) ao caminho relativo
-// $breadcrumbs .= "index.php";
-
-//echo $breadcrumbs;  
-/*    $atualpage = $_SERVER['REQUEST_URI'];
-    $URI = substr($atualpage, 24);
-    $relativePath = explode('/', $URI);
-    $dirCount = count($relativePath) - 1;
-    $test;
-
-    $returnpath = function(){
-        $test = str_repeat("../", $dirCount);
-        print_r ($relativePath);
-    }
-
-/*
-    echo $breadcrumbs;
-
-    echo "relativeURI $relativeURI<br>";
-    echo "$atualpage<br>";
-    echo "$URI<br>";
-*/
 ?>    
