04d1cfc7d25a1422821dd715e2e3320766635f3e
Ruhan Daniel Kremes <ruhankremes@alunos.utfpr.edu.br>
2024-04-16 19:08:33 -0300

    fixed pagesauthoried

--- public/pages_authorized.php
+++ public/pages_authorized.php
@@ -3,13 +3,13 @@
     $dir= $breadcrumbs . "config/DB.php";
     require "$dir";
     $dirAcessDenied = $breadcrumbs . "public/AccessDenied.php";
-    $iterator = 1;
+    $iterator = 0;
     if($_SESSION['role'] != 'admin'){
         foreach($DB_paginas_atorizada_para_usuarios as $pagina){
-            if($pagina != $_SERVER['REQUEST_URI'] && $iterator == 2){               
-                header("Location:$dirAcessDenied");
-            }else{
+            if($pagina == $_SERVER['REQUEST_URI']) {
                 break;
+            }elseif($pagina != $_SERVER['REQUEST_URI'] && $iterator == 2){               
+                header("Location: $dirAcessDenied");
             }
             $iterator++;
         }
