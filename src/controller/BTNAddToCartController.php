ab7a7f51f6b0bdbbdecb48bd53a852f52412ccca
JuliarrCosta <Juliaraquelrc@outlook.com>
2024-04-16 11:41:45 -0300

    Ajuste do BTN controller

--- src/controller/BTNAddToCartController.php
+++ src/controller/BTNAddToCartController.php
@@ -7,6 +7,12 @@ $cartController = new CartController();
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_to_cart'])) {
     // Processar a adição do produto ao carrinho
     $code = $_POST['product_code'];
+    
+    $index = array_search($code, $DB_PRODUTOS['code']);
+    $name_product = $DB_PRODUTOS['name_product'][$index];
+    $quantity_product = $DB_PRODUTOS['quantity_product'][$index];
+    $price_product = $DB_PRODUTOS['price_product'][$index];
+
     $cartController->addCart($code, $name_product, $quantity_product, $price_product);
     $addItem();
     // Redirecionar para o CartView.php
