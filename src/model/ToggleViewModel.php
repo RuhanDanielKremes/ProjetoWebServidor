a60c8fbad7fbce352221d7499104b2614938a373
Ruhan Daniel Kremes <ruhankremes@alunos.utfpr.edu.br>
2024-06-11 12:10:48 -0300

    Removed Workspace for the new project
    Removed Workspace for the new project

--- src/model/ToggleViewModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    include "../../public/breadcrumbs.php";
-    $_SESSION['view'] = ($_SESSION['view'] == "admin") ? "user" : "admin";
-    $dir = $breadcrumbs . "src/index.php";
-    header("Location: $dir");
-?>
